9496845676ac995c7bb42a7f00fb6fdc93f553fb
Frederic Ruget <frederic_ruget@tmp1.europe-west9-b.c.lab-res-fred-shared.internal>
2023-02-06 14:09:32 +0000

    -

--- src/rand.go
+++ src/rand.go
@@ -3,12 +3,17 @@ package main
 import (
 	"os"
 
+        "flag"
+
 	"github.com/golang/glog"
 	"github.com/google/go-tpm/tpm2"
 )
 
 func main() {
-	f, err := os.OpenFile("/dev/tpmrmm0", os.O_RDWR, 0)
+	flag.Parse()
+	glog.V(2).Infof("======= Init  ========")
+
+	f, err := os.OpenFile("/dev/tpmrm0", os.O_RDWR, 0)
 	if err != nil {
 		glog.Fatalf("opening tpm: %v", err)
 	}
@@ -18,5 +23,5 @@ func main() {
 	if err != nil {
 		glog.Fatalf("getting random bytes: %v", err)
 	}
-	glog.V(2).Infof("%x\n", out)
+	glog.V(2).Infof("this is it: %x\n", out)
 }
