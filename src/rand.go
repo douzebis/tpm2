1846ee3ccf63609ce7dc1461a5d5737d0e4b16fa
Frederic Ruget <frederic.ruget@thalesgroup.com>
2023-02-06 05:59:29 -0800

    -

---
+++ src/rand.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/golang/glog"
+	"github.com/google/go-tpm/tpm2"
+)
+
+func main() {
+	f, err := os.OpenFile("/dev/tpmrm0", os.O_RDWR, 0)
+	if err != nil {
+		glog.Fatalf("opening tpm: %v", err)
+	}
+	defer f.Close()
+
+	out, err := tpm2.GetRandom(f, 16)
+	if err != nil {
+		glog.Fatalf("getting random bytes: %v", err)
+	}
+	glog.V(2).Infof("%x\n", out)
+}
