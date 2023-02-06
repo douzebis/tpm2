bdf8526b17df86f3b60c5630a57cf7c88fc0e5af
Frederic Ruget <frederic.ruget@thalesgroup.com>
2023-02-06 06:37:41 -0800

    -

--- src/rand.go
+++ src/rand.go
@@ -10,15 +10,14 @@ import (
 )
 
 var (
-	tpmPath = flag.String("tpm-path", "/dev/tpm0", "Path to the TPM device (character device or a Unix socket).")
+	tpmPath = flag.String("tpm-path", "/dev/tpmrm0", "Path to the TPM device (character device or a Unix socket).")
 )
 
 func main() {
 	flag.Parse()
-	glog.V(2).Infof("======= Init  ========")
-	glog.Fatalf("tmpPath is: %s", *tpmPath)
+	glog.V(2).Infof("tmpPath is: %s", *tpmPath)
 
-	f, err := os.OpenFile("/dev/tpmrm0", os.O_RDWR, 0)
+	f, err := os.OpenFile(*tpmPath, os.O_RDWR, 0)
 	if err != nil {
 		glog.Fatalf("opening tpm: %v", err)
 	}
@@ -28,5 +27,5 @@ func main() {
 	if err != nil {
 		glog.Fatalf("getting random bytes: %v", err)
 	}
-	glog.V(2).Infof("this is it: %x\n", out)
+	glog.V(2).Infof("random bytes are: %x\n", out)
 }
